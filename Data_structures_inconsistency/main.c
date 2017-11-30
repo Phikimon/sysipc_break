07f1bf018127079a75c48ac3ba9041e478789ce6
Philippe Mikoyan <philippe.mikoyan@skat.systems>
2017-11-30 08:18:25 +0300

    Adapt for cover letter

--- Data_structures_inconsistency/main.c
+++ Data_structures_inconsistency/main.c
@@ -7,19 +7,15 @@
 
 int main(void)
 {
-    int bug_to_find = getchar();
-    int shall_child_sleep = getchar();
     while (1) {
-        int fork_ret = fork();
+        int chld_pid = fork();
         int shmid = -1;
         struct shmid_ds ds = {0};
-        if (fork_ret > 0) {
-            shmid = shmget(fork_ret, 65536, IPC_CREAT|0666);
-            while (ds.shm_nattch < 1) {
+        if (chld_pid > 0) {
+            shmid = shmget(chld_pid, 1, IPC_CREAT|0666);
+            while (ds.shm_nattch < 1)
                 shmctl(shmid, IPC_STAT, &ds);
-            }
-            if ( ((ds.shm_atime == 0 || ds.shm_lpid == 0) && (bug_to_find == 'l')) ||
-                 (        (ds.shm_nattch > 1)             && (bug_to_find == 'n'))  ) {
+            if  (ds.shm_atime == 0 || ds.shm_lpid == 0) {
                 printf("Bug found! shm_atime = %d; lpid = %d; nattch = %d\n",
                                    ds.shm_atime, ds.shm_lpid, ds.shm_nattch);
                 return 0;
@@ -27,11 +23,10 @@ int main(void)
             shmctl(shmid, 0, IPC_RMID);
         } else {
             do {
-                shmid = shmget(getpid(), 65536, 0);
+                shmid = shmget(getpid(), 1, 0);
             } while (shmid < 0);
             shmat(shmid, NULL, 0);
-            if (shall_child_sleep != '0')
-                sleep(1);
+            usleep(100000); //< To load your CPUs
             return 0;
         }
     }
---
+++ Wrong_shm_nattch_value/main.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/ipc.h"
+#include "sys/shm.h"
+
+int main(void)
+{
+    while (1) {
+        int chld_pid = fork();
+        int shmid = -1;
+        struct shmid_ds ds = {0};
+        if (chld_pid > 0) {
+            shmid = shmget(chld_pid, 1, IPC_CREAT|0666);
+            while (ds.shm_nattch < 1)
+                shmctl(shmid, IPC_STAT, &ds);
+            if  (ds.shm_nattch == 2) {
+                printf("Bug found! shm_atime = %d; lpid = %d; nattch = %d\n",
+                                   ds.shm_atime, ds.shm_lpid, ds.shm_nattch);
+                return 0;
+            }
+            shmctl(shmid, 0, IPC_RMID);
+        } else {
+            do {
+                shmid = shmget(getpid(), 1, 0);
+            } while (shmid < 0);
+            shmat(shmid, NULL, 0);
+            usleep(100000); //< To load your CPUs
+            return 0;
+        }
+    }
+}
